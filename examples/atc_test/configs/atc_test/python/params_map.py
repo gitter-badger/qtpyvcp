b7d079ebe5ace68ea1c5bea6d7023c3c7edf351c
TurBoss <j.l.toledano.l@gmail.com>
2019-04-06 07:20:11 +0200

    more info shown

--- examples/atc_test/configs/atc_test/python/params_map.py
+++ examples/atc_test/configs/atc_test/python/params_map.py
@@ -6,10 +6,18 @@ import interpreter
 
 def __init__(self):
     print("__init__")
+    emc_methods = [name for name, val in interpreter.__dict__.iteritems() if callable(val)]
+    interp_methods = [name for name, val in interpreter.__dict__.iteritems() if callable(val)]
 
-    methods = [name for name, val in interpreter.__dict__.iteritems() if callable(val)]
+    print(emc_methods)
+    print(interp_methods)
 
-    print(methods)
+    print(self.params[5190])
+    print(self.params[5191])
+    print(self.params[5192])
+    print(self.params[5193])
+    print(self.params[5194])
+    print(self.params[5195])
 
     if hasattr(interpreter, 'this'):
         if self is not interpreter.this:
